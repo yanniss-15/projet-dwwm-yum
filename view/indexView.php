45540bbdf7ab97f58046c536f53e10da3791467e
yanniss-15 <151735446+yanniss-15@users.noreply.github.com>
2024-06-23 16:28:07 +0200

    add hover on nav bar a

--- view/indexView.php
+++ view/indexView.php
@@ -1,7 +1,7 @@
-
+<?php require ('navBar.php')?>
 <body>
-<section>
-  <article>
+<section class="container">
+  <article class="indexView-content">
     <h1>Bienvenue sur <span>Yum</span></h1>
     <p>Découvrez la culture japonaise à travers diverses recettes</p>
   </article>
